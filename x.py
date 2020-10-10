80f094db6aff4faf50c3e55f745152abe1af8cb4
mudisabah <moha1858@stud.kea.dk>
2020-10-10 03:45:05 +0200

    x

---
+++ x.py
@@ -0,0 +1,18 @@
+import time
+import turtle
+from turtle import Turtle
+from random import randint
+
+# WINDOW SETUP
+window = turtle.Screen()
+window.title("NINJA TURTLES")
+turtle.bgcolor("forestgreen")
+turtle.color("white")
+turtle.speed(0)
+turtle.penup()
+turtle.setpos(-140, 200)
+turtle.write("TURTLE RACE", font=("Arial", 30, "bold"))
+turtle.penup()
+
+
+time.sleep(15)
